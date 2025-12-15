572d3fa931c7441be19fd483c27de5586960ae45
fabio-gravity <fabiofarhat19@gmail.com>
2025-12-15 02:51:24 +0000

    1

---
+++ app/sobre/page.tsx
@@ -0,0 +1,23 @@
+import MagiaDoJSX from '@/components/MagiaDoJSX/MagiaDoJSX'
+
+export default function page() {
+    return (
+        <div>
+            <h2>Interfaces Modernos</h2>
+            <p>Bem vindo À minha app em REact e Next.js.</p>
+            <MagiaDoJSX />
+        </div>
+    )
+}
+export default function Page() {
+    return (
+        <>
+            <h2>Desenvolvimento Web Moderno</h2>
+            <ul>
+                <li>React e Next.js revolucionaram a criação de interfaces web.</li>
+                <li>São usados por empresas como Facebook, Netflix e Airbnb.</li>
+                <li>Dominar estas tecnologias abre muitas oportunidades!</li>
+            </ul>
+        </>
+    )
+}
